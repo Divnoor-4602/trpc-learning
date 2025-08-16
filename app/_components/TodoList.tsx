be94c77785a6c69e6ee9377e1b3f4dee04ad6dd2
Divnoor <divnoorsingh4602@gmail.com>
2025-08-15 17:55:31 -0700

    Add @types/better-sqlite3 dependency; implement TodoList component with createTodo functionality and integrate database operations in server

--- app/_components/TodoList.tsx
+++ app/_components/TodoList.tsx
@@ -1,13 +1,31 @@
 "use client";
 
 import { trpc } from "../_trpc/client";
+import { useState } from "react";
 
 export default function TodoList() {
   const getTodos = trpc.getTodos.useQuery();
+  const createTodo = trpc.createTodo.useMutation();
+
+  const [content, setContent] = useState("");
 
   return (
     <>
       <div>{JSON.stringify(getTodos.data)}</div>
+      <input
+        type="text"
+        value={content}
+        className="border-2 border-gray-300 rounded-md p-2"
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          createTodo.mutate({ content });
+          setContent("");
+        }}
+      >
+        Add
+      </button>
     </>
   );
 }
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,29 @@
 import { publicProcedure, router } from "./trpc";
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+import { todos } from "@/db/schema";
+import Database from "better-sqlite3";
+import { z } from "zod";
+
+const sqlite = new Database("./sqlite.db");
+const db = drizzle(sqlite);
+
+// Run migrations when needed, not at import time
+// migrate(db, { migrationsFolder: "./drizzle" });
 
 export const appRouter = router({
   getTodos: publicProcedure.query(async () => {
-    return [10, 20, 30];
+    return await db.select().from(todos).all();
   }),
+  createTodo: publicProcedure
+    .input(z.object({ content: z.string() }))
+    .mutation(async ({ input }) => {
+      await db
+        .insert(todos)
+        .values({ content: input.content, completed: false })
+        .run();
+      return { success: true };
+    }),
 });
 
 export type AppRouter = typeof appRouter;
